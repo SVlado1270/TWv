0d0034d6fc5c5935ec1edbd8757ea5ba4affe491
otilia99 <45422133+otilia99@users.noreply.github.com>
2020-05-07 23:15:14 +0300

    model ajax cu si fara jquery pt charts + database cu exemplu de queries (si compus, si simplu)

--- database.php
+++ database.php
@@ -1,24 +1,30 @@
 <?php
-class Database{
-    private $host = "localhost";
-    private $db_name = "twdb";
-    private $username = "root";
-    private $password = "student";
-    public $conn;
+    $host = "localhost";
+    $dbName = "twdb";
+    $username = "root";
+    $password = "student";
+    $conn;
   
-    public function getConnection(){
-  
-        $this->conn = null;
-  
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-        }catch(PDOException $exception){
-            echo "Connection failed: " . $exception->getMessage();
+    $conn = new mysqli($host, $username, $password, $dbName);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      $criterion = "IASI";
+      //$sqlQuery = "SELECT `numar total someri` FROM rata_mai WHERE JUDET = '" . $criterion . "'";
+      $sqlQuery = "SELECT COUNT(`numar total someri`) FROM rata_mai";
+      $output = array();
+      $result = $conn->query($sqlQuery);
+      
+      if ($result->num_rows > 0) {
+        foreach($result as $row) {
+          //echo "Judetul este: " . $row["JUDET"]. "<br>";
+          $output[] = $row;
+          echo json_encode($output);
         }
-  
-        return $this->conn;
-    }
-}
-echo "Successfully connected";
+      } else {
+        //echo "No match found for your query!";
+        die("Issue with your connection found". mysqli_error($connection));
+      }
+      $conn->close();
 ?>
