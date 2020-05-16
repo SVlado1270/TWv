e7d99cb4696a85f193e4f4b991bf7817908d8eaf
vladluc99 <62203002+vladluc99@users.noreply.github.com>
2020-05-16 16:12:33 +0300

    Regiuni+judete
    Pagina regiune functioneaza corespunzator pentru fiecare regiune in parte

---
+++ selectieJudete.js
@@ -0,0 +1,71 @@
+function selectareIasi() {
+    var url = 'https://vvv1270.azurewebsites.net/IASI';
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var json = JSON.parse(this.response);
+            console.log(json)
+            sessionStorage.setItem('judet', json.Judet);
+            sessionStorage.setItem('regiune', 'NordEst');
+            /*location.replace("Judet.html");*/
+            console.log(json.Judet);
+        }
+    };
+    xhttp.open("GET", url, false);
+    xhttp.send();
+
+}
+
+function selectareBacau() {
+    var url = 'https://vvv1270.azurewebsites.net/BACAU';
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var json = JSON.parse(this.response);
+            console.log(json)
+            sessionStorage.setItem('judet', json.Judet);
+            sessionStorage.setItem('regiune', 'NordEst');
+            /*location.replace("Judet.html");*/
+            console.log(json.Judet);
+        }
+    };
+    xhttp.open("GET", url, false);
+    xhttp.send();
+
+}
+
+function selectareAlba() {
+    var url = 'https://vvv1270.azurewebsites.net/ALBA';
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var json = JSON.parse(this.response);
+            console.log(json)
+            sessionStorage.setItem('judet', json.Judet);
+            sessionStorage.setItem('regiune', 'Centru');
+            /*location.replace("Judet.html");*/
+            console.log(json.Judet);
+        }
+    };
+    xhttp.open("GET", url, false);
+    xhttp.send();
+
+}
+
+function selectareBrasov() {
+    var url = 'https://vvv1270.azurewebsites.net/BRASOV';
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var json = JSON.parse(this.response);
+            console.log(json)
+            sessionStorage.setItem('judet', json.Judet);
+            sessionStorage.setItem('regiune', 'Centru');
+            /*location.replace("Judet.html");*/
+            console.log(json.Judet);
+        }
+    };
+    xhttp.open("GET", url, false);
+    xhttp.send();
+
+}
