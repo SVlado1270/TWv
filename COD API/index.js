2ac508dcf5a63545fa8c2c318b54a181044e8e85
vlado1270 <andreivladserban@gmail.com>
2020-05-17 19:00:34 +0300

    api

--- COD API/index.js
+++ COD API/index.js
@@ -6,7 +6,8 @@ const connection = mysql.createConnection({
     host: process.env.dbHost,
     user: process.env.dbUser,
     password: process.env.dbPassword,
-    database: process.env.dbName
+    database: process.env.dbName,
+    multipleStatements: true
 })
 
 connection.connect(function (err) {
@@ -30,349 +31,36 @@ connection.connect(function (err) {
     
 });*/
 
+
 var server = http.createServer(function (req, res) {
     console.log('request was made: ' + req.url);
 
     // incarcare pagini
 
-    if(req.url === '/IASI'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Iasi'
-        }
-        res.end(JSON.stringify(obj));
-    }
+    var criterii2 = ['educatie', 'varste', 'rata', 'medii']
+  var judete2 = ['IASI', 'BOTOSANI', 'BACAU', 'VASLUI', 'GALATI', 'TULCEA', 'CONSTANTA', 'IALOMITA', 'BRAILA', 'VRANCEA', 'NEAMT', 'SUCEAVA',  'BISTRITA', 'MARAMURES', 'MURES',  'HARGHITA',  'COVASNA',  'BUZAU', 'PRAHOVA',  'ILFOV', 'DAMBOVITA', 'BRASOV', 'GIURGIU', 'TELEORMAN', 'OLT', 'DOLJ', 'ALBA',  'MEHEDINTI', 'VALCEA', 'ARGES', 'SIBIU', 'CLUJ', 'SALAJ', 'BIHOR', 'ARAD', 'TIMIS', 'CARAS',  'HUNEDOARA', 'BUCURESTI', 'CARAS', 'SATUMARE', 'CALARASI']
+    for (var jud of judete2) {
+      if (req.url === '/' + jud) {
+          var obj = {
+              Judet: jud
+          }
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+            var temp = 'SELECT * from varste WHERE judet = ' + mysql.escape(jud) + '; SELECT * from rata WHERE judet = ' + mysql.escape(jud) + '; SELECT * from educatie WHERE judet = ' + mysql.escape(jud) + '; SELECT * from rata WHERE judet = ' + mysql.escape(jud)
 
-    if(req.url === '/BACAU'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Bacau'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/BOTOSANI'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Botosani'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/SUCEAVA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Suceava'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/NEAMT'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Neamt'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/VASLUI'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Vaslui'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/GALATI'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Galati'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/VRANCEA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Vrancea'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/BRAILA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Braila'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/TULCEA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Tulcea'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/CONSTANTA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Constanta'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/IALOMITA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Ialomita'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/CALARASI'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Calarasi'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/BUZAU'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Buzau'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/GIURGIU'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Giurgiu'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/ILFOV'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Ilfov'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/BUCURESTI'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Bucuresti'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/PRAHOVA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Prahova'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/BRASOV'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Brasov'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/COVASNA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Covasna'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/HARGHITA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Harghita'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/TELEORMAN'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Teleorman'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/DAMBOVITA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Dambovita'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/MURES'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Mures'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/BISTRITA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Bistrita'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/MARAMURES'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Maramures'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/SATUMARE'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'SatuMare'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/SALAJ'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Salaj'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/CLUJ'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Cluj'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/ALBA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Alba'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/SIBIU'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Sibiu'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/VALCEA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Valcea'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/ARGES'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Arges'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/OLT'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Olt'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/DOLJ'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Dolj'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/MEHEDINTI'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Mehedinti'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/GORJ'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Gorj'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/BIHOR'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Bihor'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/ARAD'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Arad'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/TIMIS'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Timis'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/HUNEDOARA'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Hunedoara'
-        }
-        res.end(JSON.stringify(obj));
-    }
-
-    if(req.url === '/CARAS'){
-        res.writeHead(200,{'Content-type': 'application/json'});
-        var obj = {
-            Judet: 'Caras'
-        }
-        res.end(JSON.stringify(obj));
+        connection.query(temp, (err, results, fields) => {
+          if (err) throw err
+          var test = JSON.stringify(results)
+          var rez = JSON.parse(test)
+          result = []
+          result.push(obj)
+          result.push(rez)
+          res.end(JSON.stringify(result))
+        })
+      }
     }
 
     var criterii = ['educatie', 'varste', 'rata', 'medii']
-  var judete = ['IASI', 'BOTOSANI', 'BACAU', 'VASLUI', 'GALATI', 'TULCEA', 'CONSTANTA', 'IALOMITA', 'BRAILA', 'VRANCEA', 'NEAMT', 'SUCEAVA',  'BISTRITA', 'MARAMURES', 'MURES',  'HARGHITA',  'COVASNA',  'BUZAU', 'PRAHOVA',  'ILFOV', 'DAMBOVITA', 'BRASOV', 'GIURGIU', 'TELEORMAN', 'OLT', 'DOLJ', 'ALBA',  'MEHEDINTI', 'VALCEA', 'ARGES', 'SIBIU', 'CLUJ', 'SALAJ', 'BIHOR', 'ARAD', 'TIMIS', 'CARAS',  'HUNEDOARA', 'BUCURESTI', 'CARAS', 'SATUMARE']
+  var judete = ['IASI', 'BOTOSANI', 'BACAU', 'VASLUI', 'GALATI', 'TULCEA', 'CONSTANTA', 'IALOMITA', 'BRAILA', 'VRANCEA', 'NEAMT', 'SUCEAVA',  'BISTRITA', 'MARAMURES', 'MURES',  'HARGHITA',  'COVASNA',  'BUZAU', 'PRAHOVA',  'ILFOV', 'DAMBOVITA', 'BRASOV', 'GIURGIU', 'TELEORMAN', 'OLT', 'DOLJ', 'ALBA',  'MEHEDINTI', 'VALCEA', 'ARGES', 'SIBIU', 'CLUJ', 'SALAJ', 'BIHOR', 'ARAD', 'TIMIS', 'CARAS',  'HUNEDOARA', 'BUCURESTI', 'CARAS', 'SATUMARE', 'CALARASI']
   for (var criteriu of criterii) {
     for (var judet of judete) {
       if (req.url === '/' + judet + '/' + criteriu) {
